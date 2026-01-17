f3d05f260e8ae4538b9f1f4d8b24b42ace83d230
Sevims Office <121771333+SevimsOffice@users.noreply.github.com>
2026-01-17 22:51:56 +0300

    Update src/App.tsx to change HashRouter to BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
-import { BrowserRouter as Router } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { LanguageProvider } from './contexts/LanguageContext';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomePage from './pages/HomePage';
+import TrainingsPage from './pages/TrainingsPage';
+import BlogsPage from './pages/BlogsPage';
+import ProductsPage from './pages/ProductsPage';
 
 function App() {
-    return (
-        <Router>
-            {/* Your routes go here */}
-        </Router>
-    );
+  return (
+    <LanguageProvider>
+      <Router>
+        <div className="min-h-screen bg-gray-950 text-white">
+          <Header />
+          <main>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/trainings" element={<TrainingsPage />} />
+              <Route path="/blogs" element={<BlogsPage />} />
+              <Route path="/products" element={<ProductsPage />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </Router>
+    </LanguageProvider>
+  );
 }
 
 export default App;
